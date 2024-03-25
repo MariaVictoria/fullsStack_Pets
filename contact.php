b62b9e8259435ac1d3fda11e5ff50f84fa85494d
María Victoria <victoriacursos2022@gmail.com>
2024-03-24 22:48:43 -0300

    actualizao con ampps

--- contact.php
+++ contact.php
@@ -1,22 +1,21 @@
 <?php
-$nombre= $_POST ['name'];
-$emai =$_POST['email'];
-$asunto= $_POST['subject'];
-$mensaje= $_POST ['message'];
+$nombre = $_POST['name'];
+$email = $_POST['email'];
+$asunto = $_POST['subject'];
+$mensaje = $_POST['message'];
 
-$mensaje = ' Este mensaje fue enviado por : ' . $nombre . ",\r\n";
-$mensaje = " Su email es : " . $emai. ",\r\n";
-$mensaje = " El asunto es : " . $asunto . ",\r\n";
-$mensaje = " El mensaje es :" . $_POST . ",\r\n";
-$mensaje = " Enviado el día : " . date('d/m/y' , time());
+$mensaje = 'Este mensaje fue enviado por: ' . $nombre . ",\r\n";
+$mensaje .= "Su email es: " . $email . ",\r\n";
+$mensaje .= "El asunto es: " . $asunto . ",\r\n";
+$mensaje .= "El mensaje es: " . $mensaje . ",\r\n"; // Aquí deberías usar la variable $mensaje que has definido arriba, no $_POST
+$mensaje .= "Enviado el día: " . date('d/m/y', time());
 
 $para = 'victoriacursos2022@gmail.com';
-$asunto =  'mensaje del formulario de "PETS" '
+$asunto = 'Mensaje del formulario de "PETS"';
 
 // funcion propia de php
-mail ($para , $asunto , utf8_decode($mensaje), $header);
+mail($para, $asunto, utf8_decode($mensaje));
 
 //header es redireccionamiento
-
-header('Location : exito.html')
+header('Location: exito.html');
 ?>
